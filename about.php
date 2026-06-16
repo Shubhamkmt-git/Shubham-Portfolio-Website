864e2f70b48142438881775bd65f444d57cff813
Shubham <shubham332709@gmail.com>
2026-06-16 21:54:02 +0530

    Update about

--- about.php
+++ about.php
@@ -14,7 +14,7 @@
         About&nbsp;me<span class="text-[#ebdbf2]">.</span>
     </p></div>
     
-    <div class="px-2 fade-left">
+    <!-- <div class="px-2 fade-left">
        <p class="text-xl md:text-3xl text-[#ebdbf2]  md:text-justify items-center px-0 md:px-10 ">I am a passionate developer dedicated to creating modern and user-focused digital experiences. Combining design and development, I turn ideas into scalable, high-performing, and visually refined applications. <br>
     <br>
 Skilled in <br>
@@ -22,7 +22,23 @@ Skilled in <br>
  <br><br>
  I focus on building efficient, responsive, and maintainable solutions. I continuously learn and adapt to new technologies to deliver work that balances performance, creativity, and precision.
 </p> 
-    </div>
+    </div> -->
+    <div class="px-2 ">
+  <p class="text-xl md:text-2xl text-[#ebdbf2] md:text-justify items-center px-0 md:px-10 fade-left">
+    I am a passionate React and Laravel developer dedicated to building modern, user-focused digital experiences. With a Bachelor of Computer Applications (BCA) and a strong foundation in web development, I transform ideas into scalable, high-performance, and visually engaging applications.
+    Currently working as a React-Laravel Developer at Saalo Tech, Jaipur.
+    <br><br>
+
+    Skilled in <br>
+    <span class="font-semibold ">
+      HTML5, CSS3, Tailwind CSS, JavaScript, React.js, Bootstrap, PHP, Laravel, MySQL, Git, GitHub, Postman, Figma and Canva.
+    </span>
+
+    <br><br>
+
+    I focus on creating efficient, responsive, and maintainable solutions while continuously learning new technologies and industry best practices. My goal is to deliver products that balance performance, creativity, and exceptional user experience.
+  </p>
+</div>
 </section>
 
 
